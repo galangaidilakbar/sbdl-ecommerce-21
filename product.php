6272af828cb8e983059e274411bec2dcbcedb670
Galangaidil <galangaidil45@gmail.com>
2021-05-21 22:35:20 +0700

    Implementing stored procedure

--- product.php
+++ product.php
@@ -2,7 +2,7 @@
 // Check to make sure the id parameter is specified in the URL
 if (isset($_GET['id'])) {
   // Prepare statement and execute, prevents SQL injection
-  $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
+  $stmt = $pdo->prepare('CALL tampilkan_produk(?)');
   $stmt->execute([$_GET['id']]);
   // Fetch the product from the database and return the result as an Array
   $product = $stmt->fetch(PDO::FETCH_ASSOC);
