c31d0d24a0f99c3706d21f64a75c0ec68dbc6b48
Galangaidil <galangaidil45@gmail.com>
2021-05-21 18:26:26 +0700

    Implementing view recently_added to home.php

--- home.php
+++ home.php
@@ -1,6 +1,6 @@
 <?php
 // Get the 4 most recently added products
-$stmt = $pdo->prepare('SELECT * FROM products ORDER BY date_added DESC LIMIT 4');
+$stmt = $pdo->prepare('SELECT * FROM `recently_added`');
 $stmt->execute();
 $recently_added_products = $stmt->fetchAll(PDO::FETCH_ASSOC);
 ?>
