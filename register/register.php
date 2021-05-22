182cec18b07e091e44c126422d8c8e181529d03d
Galangaidil <galangaidil45@gmail.com>
2021-05-23 00:10:49 +0700

    add role "costumer"

--- register/register.php
+++ register/register.php
@@ -32,16 +32,16 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
 		echo 'Username exists, please choose another!';
 	} else {
 		// Username doesnt exists, insert new account
-if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALUES (?, ?, ?)')) {
-	// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
-	$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
-	$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
-	$stmt->execute();
-	header('Location: ../phplogin/index.php');
-} else {
-	// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
-	echo 'Could not prepare statement!';
-}
+		if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, role) VALUES (?, ?, ?, "costumer")')) {
+			// We do not want to expose passwords in our database, so hash the password and use password_verify when a user logs in.
+			$password = password_hash($_POST['password'], PASSWORD_DEFAULT);
+			$stmt->bind_param('sss', $_POST['username'], $password, $_POST['email']);
+			$stmt->execute();
+			header('Location: ../phplogin/index.php');
+		} else {
+			// Something is wrong with the sql statement, check to make sure accounts table exists with all 3 fields.
+			echo 'Could not prepare statement!';
+		}
 	}
 	$stmt->close();
 } else {
@@ -49,4 +49,3 @@ if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email) VALU
 	echo 'Could not prepare statement!';
 }
 $con->close();
-?>
