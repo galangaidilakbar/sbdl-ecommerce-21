be27143b25bddd87f1adc37fc04267ec98ee03e7
Galangaidil <galangaidil45@gmail.com>
2021-05-22 23:47:23 +0700

    add login admin

--- phplogin/authenticate.php
+++ phplogin/authenticate.php
@@ -10,7 +10,7 @@ if (!isset($_POST['username'], $_POST['password'])) {
   exit('Please fill both the username and password fields!');
 }
 // Prepare our SQL, preparing the SQL statement will prevent SQL injection.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+if ($stmt = $con->prepare('SELECT id, password, role FROM accounts WHERE username = ?')) {
   // Bind parameters (s = string, i = int, b = blob, etc), in our case the username is a string so we use "s"
   $stmt->bind_param('s', $_POST['username']);
   $stmt->execute();
@@ -18,7 +18,7 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
   $stmt->store_result();
 
   if ($stmt->num_rows > 0) {
-    $stmt->bind_result($id, $password);
+    $stmt->bind_result($id, $password, $role);
     $stmt->fetch();
     // Account exists, now we verify the password.
     // Note: remember to use password_hash in your registration file to store the hashed passwords.
@@ -29,7 +29,13 @@ if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?'
       $_SESSION['loggedin'] = TRUE;
       $_SESSION['name'] = $_POST['username'];
       $_SESSION['id'] = $id;
-      header('Location: ../admin/index.php');
+      var_dump($role);
+      var_dump($id);
+      if ($role == 'admin') {
+        header('Location: ../admin/index.php');
+      } else {
+        header('../index.php');
+      }
     } else {
       // Incorrect password
       echo 'Incorrect username and/or password!';
