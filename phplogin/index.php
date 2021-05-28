dcc542c2c45a7b362c69b5d7239b3ca2c65aa06c
Galangaidil <galangaidil45@gmail.com>
2021-05-28 14:56:29 +0700

    remove unnecessary file

--- phplogin/index.php
+++ phplogin/index.php
@@ -7,7 +7,7 @@ include 'functions.php';
 
 <head>
 	<meta charset="utf-8">
-	<title>Login</title>
+	<title>Login | Gadgets Pedia</title>
 	<link href="style.css" rel="stylesheet" type="text/css">
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	<?= load_icon() ?>
