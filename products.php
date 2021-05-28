80bf0e2d1863c0009c4943db27eff1403b2c3b7d
Galangaidil <galangaidil45@gmail.com>
2021-05-28 14:02:46 +0700

    add products and 8 products in 1 pages

--- products.php
+++ products.php
@@ -1,6 +1,6 @@
 <?php
 // The amounts of products to show on each page
-$num_products_on_each_page = 4;
+$num_products_on_each_page = 8;
 // The current page, in the URL this will appear as index.php?page=products&p=1, index.php?page=products&p=2, etc...
 $current_page = isset($_GET['p']) && is_numeric($_GET['p']) ? (int)$_GET['p'] : 1;
 // Select products ordered by the date added
