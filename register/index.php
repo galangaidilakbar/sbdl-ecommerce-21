e9fd303363648d7767a0ef65183f23350934dce8
Galangaidil <galangaidil45@gmail.com>
2021-05-31 09:56:08 +0700

    fixing when register with same email

--- register/index.php
+++ register/index.php
@@ -4,12 +4,15 @@
 <head>
 	<meta charset="utf-8">
 	<title>Register | Gadgets Pedia</title>
+
+	<!-- Icon and favicon start -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	<link href="style.css" rel="stylesheet" type="text/css">
 	<link rel="apple-touch-icon" sizes="180x180" href="../imgs/icon/apple-touch-icon.png">
 	<link rel="icon" type="image/png" sizes="32x32" href="../imgs/icon/favicon-32x32.png">
 	<link rel="icon" type="image/png" sizes="16x16" href="../imgs/icon/favicon-16x16.png">
 	<link rel="manifest" href="../imgs/icon/site.webmanifest">
+	<!-- Icon and favicon End -->
 </head>
 
 <body>
@@ -28,13 +31,13 @@
 				<label for="email">
 					<i class="fas fa-envelope"></i>
 				</label>
-				<input type="email" name="email" placeholder="Email" id="email" required>
-				<label for="password">
+				<input type="email" name="email" placeholder="Email" id="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$" required>
+				<label for="password1">
 					<i class="fas fa-lock"></i>
 				</label>
 				<input type="password" name="password1" placeholder="Password" id="password1" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 8 or more characters" required>
-				<label for="password">
-					<i class="fas fa-lock"></i>
+				<label for="password2">
+					<i class="fas fa-check-circle"></i>
 				</label>
 				<input type="password" name="password2" placeholder="Confirm password" id="password2" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}" title="Your password must same with password above" required>
 				<input type="submit" value="Register">
--- register/register.php
+++ register/register.php
@@ -1,48 +1,39 @@
 <?php
+// Include and connect to database
 include '../phplogin/functions.php';
 $con = mysqli_connect_to_database();
 
-if (mysqli_connect_errno()) {
-	// If there is an error with the connection, stop the script and display the error.
-	exit('Failed to connect to MySQL: ' . mysqli_connect_error());
-}
 // Now we check if the data was submitted, isset() function will check if the data exists.
 if (!isset($_POST['username'], $_POST['password1'], $_POST['password2'], $_POST['email'])) {
 	// Could not get the data that should have been sent.
-	exit('Please complete the registration form!');
+	create_alert_message('Please complete the registration form!');
 }
 // Make sure the submitted registration values are not empty.
 if (empty($_POST['username']) || empty($_POST['password1']) || empty($_POST['password2']) || empty($_POST['email'])) {
 	// One or more values are empty.
-	exit('Please complete the registration form');
+	create_alert_message('Please complete the registration form');
 }
 
 //Make sure the password lenght is 8 character
 if (strlen($_POST['password1']) <= 8) {
-	exit('Your password must be have at least <br>
-		<ul>
-			<li>8 characters long</li>
-			<li>1 uppercase & 1 lowercase character</li>
-			<li>1 number</li>
-		</ul>
-	');
+	create_alert_message('Your password must be have at least 8 characters long, 1 uppercase & 1 lowercase character, 1 number');
 }
 
 //Make sure password and confirm password are same
 if ($_POST['password1'] != $_POST['password2']) {
-	exit('Comfirm password are not same');
+	create_alert_message('Comfirm password are not same');
 }
 
-// We need to check if the account with that username exists.
-if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ?')) {
+// We need to check if the account with that username or email exists.
+if ($stmt = $con->prepare('SELECT id, password FROM accounts WHERE username = ? OR email = ?')) {
 	// Bind parameters (s = string, i = int, b = blob, etc), hash the password using the PHP password_hash function.
-	$stmt->bind_param('s', $_POST['username']);
+	$stmt->bind_param('ss', $_POST['username'], $_POST['email']);
 	$stmt->execute();
 	$stmt->store_result();
 	// Store the result so we can check if the account exists in the database.
 	if ($stmt->num_rows > 0) {
-		// Username already exists
-		echo 'Username exists, please choose another!';
+		// Username / Email already exists
+		create_alert_message('Username / Email has taken, Please choose another!');
 	} else {
 		// Username doesnt exists, insert new account
 		if ($stmt = $con->prepare('INSERT INTO accounts (username, password, email, role, code) VALUES (?, ?, ?, "costumer", ?)')) {
@@ -79,4 +70,14 @@ function send_verification_email($email, $code)
 
 	$headers = 'From: galangaidil45@gmail.com' . "\r\n";
 	return mail($to, $subject, $message, $headers);
+}
+
+function create_alert_message($message)
+{
+	echo "<script>
+					alert('$message');
+					document.location.href = '.';
+        </script>
+        ";
+	exit();
 }
