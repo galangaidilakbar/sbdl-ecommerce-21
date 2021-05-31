8611bd545c4cf8bf7f9b2337aa7a4ddd3de3ea68
Galangaidil <galangaidil45@gmail.com>
2021-05-31 23:33:50 +0700

    create docs

--- databases/sql.sql
+++ databases/sql.sql
@@ -1,6 +1,9 @@
+-- fisrt we need to create database, in this case, we create database named shoppingcart
 CREATE DATABASE shoppingcart;
+-- then we use the shoppingcart database
 USE shoppingcart;
 
+-- create table products
 CREATE TABLE IF NOT EXISTS `products` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`name` VARCHAR(200) NOT NULL,
@@ -13,31 +16,53 @@ CREATE TABLE IF NOT EXISTS `products` (
 	PRIMARY KEY (`id`)
 ) ENGINE=INNODB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8;
 
-INSERT INTO `products` (`id`, `name`, `desc`, `price`, `rrp`, `quantity`, `img`, `date_added`) VALUES
-(1, 'Smart Watch', '<p>Unique watch made with stainless steel, ideal for those that prefer interative watches.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Powered by Android with built-in apps.</li>\r\n<li>Adjustable to fit most.</li>\r\n<li>Long battery life, continuous wear for up to 2 days.</li>\r\n<li>Lightweight design, comfort on your wrist.</li>\r\n</ul>', '29.99', '0.00', 10, 'watch.jpg', '2019-03-13 17:55:22'),
-(2, 'Wallet', '', '14.99', '19.99', 34, 'wallet.jpg', '2019-03-13 18:52:49'),
-(3, 'Headphones', '', '19.99', '0.00', 23, 'headphones.jpg', '2019-03-13 18:47:56'),
-(4, 'Digital Camera', '', '69.99', '0.00', 7, 'camera.jpg', '2019-03-13 17:42:04');
-
+-- create table accounts
 CREATE TABLE IF NOT EXISTS `accounts` (
 	`id` INT(11) NOT NULL AUTO_INCREMENT,
 	`username` VARCHAR(50) NOT NULL,
   `password` VARCHAR(255) NOT NULL,
   `email` VARCHAR(100) NOT NULL,
-    PRIMARY KEY (`id`)
+	`role` VARCHAR (100) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=INNODB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 
-INSERT INTO `accounts` (`username`, `password`, `email`, `role`) VALUES ('fandi', '$2y$10$gfM5uiVqrq6yE/XUttuonuO2Z9prT6GoLzqMNpHLfXdteZj1BHqG2', 'galangaidil45@gmail.com', 'admin');
+-- create table `log_products` 
+-- this table use to see what products that inserted, and when.
+CREATE TABLE IF NOT EXISTS `log_products`(
+	`id_` INT(11),
+	`name` VARCHAR(200),
+	`kejadian` DATETIME,
+	`keterangan` TEXT
+);
 
+-- create table orders
+CREATE TABLE IF NOT EXISTS `orders`(
+	orderID INT(11) NOT NULL AUTO_INCREMENT,
+	productID INT(11) NOT NULL,
+	accountsID INT(11) NOT NULL,
+	date_created DATETIME,
+	`jumlah` INT(11) NOT NULL,
+	`total` DECIMAL(7,2) NOT NULL
+	PRIMARY KEY (orderID),
+	FOREIGN KEY (productID) REFERENCES products(id),
+	FOREIGN KEY (accountsID) REFERENCES accounts(id)
+);
 
 -- create view for select 4 the most recently added product
+-- this view will we use in home.php
 CREATE VIEW recently_added AS
 SELECT `id`, `img`, `name`, `price`, `rrp` FROM products ORDER BY date_added DESC LIMIT 4;
 
--- memanggil view --
-SELECT * FROM `recently_added`;
+-- create view to show incoming orders
+-- this view will we use in admin/orders.php
+CREATE VIEW orderan_masuk AS
+SELECT products.`name`, accounts.`username`, `orders`.`jumlah`, products.`price`, orders.`total`
+FROM `orders`
+INNER JOIN products ON orders.`productID` = `products`.`id`
+INNER JOIN accounts ON orders.`accountsID` = `accounts`.`id`;
 
--- create stored procedure to menampilkan produk
+-- create stored procedure to menampilkan produk bases on id product.
+-- this stored procedure will we use in product.php
 DELIMITER $$
 CREATE PROCEDURE tampilkan_produk (sp_id INT(11))
 BEGIN 
@@ -47,11 +72,8 @@ BEGIN
 END $$
 DELIMITER ;
 
--- memanggil stored procedure`tampilkan_produk`
--- call tampilkan_produk(parameternya);
--- drop procedure if exists `tampilkan_produk`;
-
--- create stored procedure to insert data product
+-- create stored procedure to insert new product
+-- this stored procedure will we use in admin/create.php
 DELIMITER $$
 CREATE PROCEDURE insert_new_product(
 	IN sp_id INT(11),
@@ -68,14 +90,19 @@ BEGIN
 END $$
 DELIMITER ;
 
--- create trigger when admin insert new product
-CREATE TABLE IF NOT EXISTS `log_products`(
-	`id_` INT(11),
-	`name` VARCHAR(200),
-	`kejadian` DATETIME,
-	`keterangan` TEXT
-);
+-- create procedure to search products;
+-- this store procedure will we use in function : template_header()
+DELIMITER $$
+CREATE PROCEDURE search_product (
+	IN `nama_products` VARCHAR(200)
+)
+BEGIN
+	SELECT products.`id`, products.`img`, products.`name`, products.`price`, products.`rrp` FROM products WHERE `name` LIKE CONCAT("%", `nama_products`, "%");
+END $$
+DELIMITER ;
 
+-- create trigger when admin insert new product
+-- when admin inserting new products, this trigger will insert data product to `log_products`
 DELIMITER $$
 CREATE TRIGGER log_new_products
 AFTER INSERT ON products
@@ -85,11 +112,8 @@ BEGIN
 END $$
 DELIMITER ;
 
-
-SELECT * FROM log_products;
-
--- function untuk menghitung total produk
-
+-- function to count total product
+-- this function will we use in functions.php in root, products.php, admin/index.php
 DELIMITER $$
 CREATE FUNCTION count_total_product()
 RETURNS INT(11)
@@ -101,81 +125,49 @@ BEGIN
 END $$
 DELIMITER ;
 
-SELECT count_total_product();
+-- try to inserting some values to table products
+INSERT INTO `products` (`id`, `name`, `desc`, `price`, `rrp`, `quantity`, `img`, `date_added`) VALUES
+(1, 'Smart Watch', '<p>Unique watch made with stainless steel, ideal for those that prefer interative watches.</p>\r\n<h3>Features</h3>\r\n<ul>\r\n<li>Powered by Android with built-in apps.</li>\r\n<li>Adjustable to fit most.</li>\r\n<li>Long battery life, continuous wear for up to 2 days.</li>\r\n<li>Lightweight design, comfort on your wrist.</li>\r\n</ul>', '29.99', '0.00', 10, 'watch.jpg', '2019-03-13 17:55:22'),
+(2, 'Wallet', '', '14.99', '19.99', 34, 'wallet.jpg', '2019-03-13 18:52:49'),
+(3, 'Headphones', '', '19.99', '0.00', 23, 'headphones.jpg', '2019-03-13 18:47:56'),
+(4, 'Digital Camera', '', '69.99', '0.00', 7, 'camera.jpg', '2019-03-13 17:42:04');
 
--- add column to table accounts
+-- try to insert some values to table accounts
+-- because we do not want to expose the password in our database, we use password_hash in the column `password`
+-- role has two values: admin, and costumer. each values determine where the user will go. role with admin values will go to admin pages, and role with costumes values will go to home pages.
+INSERT INTO `accounts` (`username`, `password`, `email`, `role`) VALUES ('YourName', '$2y$10$gfM5uiVqrq6yE/XUttuonuO2Z9prT6GoLzqMNpHLfXdteZj1BHqG2', 'test@example', 'admin');
 
-ALTER TABLE accounts
-ADD `role` VARCHAR (100);
+-- how to call the view `recently_added`
+SELECT * FROM `recently_added`;
 
-SELECT * FROM accounts;
-SELECT * FROM products;
-SELECT * FROM log_products;
-SELECT * FROM orders;
+-- how to call the view `orderan_masuk`
+SELECT * FROM `orderan_masuk`;
 
--- create table order 
-CREATE TABLE IF NOT EXISTS `orders`(
-	orderID INT(11),
-	productID INT(11),
-	accountsID INT(11),
-	date_created DATETIME,
-	PRIMARY KEY (orderID),
-	FOREIGN KEY (productID) REFERENCES products(id),
-	FOREIGN KEY (accountsID) REFERENCES accounts(id)
-);
+-- How to call stored procedure`tampilkan_produk`
+CALL tampilkan_produk(parameter);
 
-SELECT * FROM `orders`;
+-- how to call stored procedure `insert_new_product`
+-- ? = param
+CALL insert_new_product(?,?,?,?,?,?,?,?);
 
-INSERT INTO `orders` (productID, accountsID, date_created, jumlah)VALUES (3, 9, NOW(), 1);
+-- how to call the stored procedure search_product
+CALL search_product("Macbook");
 
-ALTER TABLE `orders`
-ADD COLUMN `jumlah` INT(11);
+-- how to drop stored procedure `tampilkan_produk`
+-- if you want to edit the stored procedure, you have to drop the stored procedure first, then edit it
+DROP PROCEDURE IF EXISTS `tampilkan_produk`;
 
-ALTER TABLE `orders`
-MODIFY `orderID` INT(11) NOT NULL AUTO_INCREMENT;
+-- how to drop stored procedure `insert_new_product`
+-- if you want to edit the stored procedure, feel free to drop the stored procedure first, then edit it
+DROP PROCEDURE IF EXISTS `insert_new_product`;
 
-ALTER TABLE `orders`
-ADD COLUMN `total` DECIMAL(7,2) NOT NULL;
-
-DELETE FROM log_products;
-
--- menampilkan nama produk, nama costumer, jumlah, dan harga berdasarkan orderID
-SELECT products.`name`, accounts.`username`, `orders`.`jumlah`, products.`price`, orders.`total`
-FROM `orders`
-INNER JOIN products ON orders.`productID` = `products`.`id`
-INNER JOIN accounts ON orders.`accountsID` = `accounts`.`id`;
-
--- add column verification code to accounts 
-ALTER TABLE accounts
-ADD COLUMN `code` INT(6);
-
--- add column active ke accounts
-ALTER TABLE accounts
-ADD COLUMN `active` INT(1) NOT NULL DEFAULT 0;
-
-UPDATE `accounts` SET `active` = 1 WHERE `accounts`.`id` = 6;
-
-SELECT * FROM accounts;
-
--- create view to show orders
-CREATE VIEW orderan_masuk AS
-SELECT products.`name`, accounts.`username`, `orders`.`jumlah`, products.`price`, orders.`total`
-FROM `orders`
-INNER JOIN products ON orders.`productID` = `products`.`id`
-INNER JOIN accounts ON orders.`accountsID` = `accounts`.`id`;
-
--- create procedure to search products;
-DELIMITER $$
-CREATE PROCEDURE search_product (
-	IN `nama_products` VARCHAR(200)
-)
-BEGIN
-	SELECT products.`id`, products.`img`, products.`name`, products.`price`, products.`rrp` FROM products WHERE `name` LIKE CONCAT("%", `nama_products`, "%");
-END $$
-DELIMITER ;
-
--- drop the search_product
+-- how to drop stored procedure `search_product`
 DROP PROCEDURE IF EXISTS search_product;
 
--- call it
-CALL search_product("Macbook");
+-- how to call the function count_total_product()
+SELECT count_total_product();
+
+-- see data that admin inserting into table products in table log_products
+SELECT * FROM log_products;
+
+-- next we will create trigger when admin update, and delete the products in table log_products
