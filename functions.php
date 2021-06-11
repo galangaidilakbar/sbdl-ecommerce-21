6c07edc72fcb9aad324f7cd0509c9f2db436d705
Galangaidil <galangaidil45@gmail.com>
2021-06-11 23:01:08 +0700

    create conn using oop

--- functions.php
+++ functions.php
@@ -1,18 +1,4 @@
 <?php
-function pdo_connect_mysql()
-{
-  // Update the details below with your MySQL details
-  $DATABASE_HOST = 'localhost';
-  $DATABASE_USER = 'yuda';
-  $DATABASE_PASS = 'tes123';
-  $DATABASE_NAME = 'shoppingcart';
-  try {
-    return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME . ';charset=utf8', $DATABASE_USER, $DATABASE_PASS);
-  } catch (PDOException $exception) {
-    // If there is an error with the connection, stop the script and display the error.
-    exit('Failed to connect to database!');
-  }
-}
 // Template header, this will showing to costumer
 function template_header($title)
 {
--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 session_start();
 // Include functions and connect to the database using PDO MySQL
 include 'functions.php';
-$pdo = pdo_connect_mysql();
+require_once "config/database.php";
+$conn = new connectDB();
+$pdo = $conn->pdo_connect();
 // Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
 // Include and show the requested page
