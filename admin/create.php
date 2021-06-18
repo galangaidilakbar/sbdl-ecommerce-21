7bd22ccc43f0fbea34e15dfbff16ebcabc709b21
Galangaidil <galangaidil45@gmail.com>
2021-06-19 03:02:24 +0700

    create conn

--- admin/create.php
+++ admin/create.php
@@ -14,7 +14,10 @@ if ($_SESSION['role'] == 'costumer') {
 }
 
 include '../functions.php';
-$pdo = pdo_connect_mysql();
+// Connect to MySQL database
+require_once "../config/database.php";
+$conn = new connectDB();
+$pdo = $conn->pdo_connect();
 $msg = '';
 // Check if POST data is not empty
 if (!empty($_POST)) {
