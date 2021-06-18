31749fc39fd88303a9d5a1888735d2e08db39378
Galangaidil <galangaidil45@gmail.com>
2021-06-19 04:03:58 +0700

    Create some comment for screenshot

--- config/database.php
+++ config/database.php
@@ -1,6 +1,9 @@
+<!-- Membuat koneksi ke database menggunakan PHP OOP -->
+
 <?php
 class connectDB
 {
+  // Sesuaikan dengan Detail MySQL mu ya!
   private $dbHost = "localhost";
   private $dbUser = "root";
   private $dbPassword = "";
@@ -8,17 +11,21 @@ class connectDB
 
   function pdo_connect()
   {
+    // Dengan PDO, kita bisa menggunakan try and catch
+  
     try {
-      $pdo = new PDO("mysql:host=$this->dbHost;dbname=$this->dbName", $this->dbUser, $this->dbPassword);
-      return $pdo;
-    } catch (PDOException $th) {
-      echo "Terjadi masalah : " . $th->getMessage() . "<br>";
-      echo "<h4>Detail Masalah</h4>";
+      return new PDO("mysql:host=$this->dbHost;dbname=$this->dbName", $this->dbUser, $this->dbPassword);
+    } catch (PDOException $e) {
+
+      // Jika ada kesalahan dengan koneksi, hentikan skrip dan tampilkan kesalahan.
+      echo "Terjadi masalah : <strong>" . $e->getMessage() . "</strong> <br>";
+      echo "<h3>Detail Masalah</h3>";
       echo "<ol>";
-      echo "<li>Kode Masalah : " . $th->getCode() . "</li>";
-      echo "<li>Lokasi File: " . $th->getFile() . "</li>";
-      echo "<li>Pada baris ke : " . $th->getLine() . "</li>";
+        echo "<li>Kode Masalah : " . $e->getCode() . "</li>";
+        echo "<li>Lokasi File: " . $e->getFile() . "</li>";
+        echo "<li>Pada baris ke : " . $e->getLine() . "</li>";
       echo "</ol>";
+      exit;
     }
   }
 }
