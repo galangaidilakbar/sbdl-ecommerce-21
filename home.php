b714036831c1df77ae247f70f1187ff36e3271f7
Galangaidil <galangaidil45@gmail.com>
2021-06-19 02:55:14 +0700

    Create Comment

--- home.php
+++ home.php
@@ -1,11 +1,34 @@
+<!-- SHOPPINGCART
+ ****************************************************
+ * Developer Team : Galang aidil akbar, Mardha Yuda Kurniawan, Ahmad Sofiyan Alfandi. 
+ * Release Date   : 24 May 2021
+ * Twitter        : https://twitter.com/galang_aidil, https://twitter.com/alfandi04_ 
+ * E-mail         : galangaidil45@gmail.com, yumardha@gmail.com, alfafandi0@gmail.com.
+-->
+
 <?php
-// Get the 4 most recently added products using view recently_added
+
+/* 
+EN : Include manage-product.php to create products class
+ID : Sertakan manage-product.php untuk membuat kelas products
+*/
 require_once "manage-product.php";
 $products = new product();
+
+/* 
+EN : Get the 4 most recently added products using function recently_added()
+ID : Dapatkan 4 produk yang paling baru ditambahkan menggunakan fungsi recently_added()
+*/
 $recently_added_products = $products->recently_added();
+
 ?>
 
-<?php if (isset($_SESSION['loggedin'])) : ?>
+<!-- 
+EN : to find out if the customer has logged in or not, we have to check session 'loggedin'. if it isset and has a value of true, then use costumer_template_header(?,?).if not, use template_header().
+
+ID : juntuk mengetahui pelanggan sudah login atau belum, kita bisa mengeceknya menggunakan session 'loginin'. jika sudah diatur dan memiliki nilai true, maka gunakan costumer_template_header(?,?). kalau belum, gunakan template_header()
+-->
+<?php if (isset($_SESSION['loggedin']) && $_SESSION['loggedin'] === TRUE) : ?>
   <?= costumer_template_header('Situs Jual Beli Gagdet No. 1 Di Indonesia', $_SESSION['name']) ?>
 <?php else : ?>
   <?= template_header('Situs Jual Beli Gagdet No. 1 Di Indonesia') ?>
@@ -15,20 +38,29 @@ $recently_added_products = $products->recently_added();
   <h2>Gadgets</h2>
   <p>Essential gadgets for everyday use</p>
 </div>
+
 <div class="recentlyadded content-wrapper">
   <h2>Recently Added Products</h2>
+
   <div class="products">
     <?php foreach ($recently_added_products as $product) : ?>
+
       <a href="index.php?page=product&id=<?= $product['id'] ?>" class="product">
+
         <img src="assets/products/<?= $product['img'] ?>" width="200" height="200" alt="<?= $product['name'] ?>">
+
         <span class="name"><?= $product['name'] ?></span>
-        <span class="price">
-          &dollar;<?= $product['price'] ?>
+
+        <span class="price"> &dollar;<?= $product['price'] ?>
+
           <?php if ($product['rrp'] > 0) : ?>
             <span class="rrp">&dollar;<?= $product['rrp'] ?></span>
           <?php endif; ?>
+
         </span>
+
       </a>
+
     <?php endforeach; ?>
   </div>
 </div>
--- index.php
+++ index.php
@@ -1,11 +1,33 @@
+<!-- SHOPPINGCART
+ ****************************************************
+ * Developer Team : Galang aidil akbar, Mardha Yuda Kurniawan, Ahmad Sofiyan Alfandi. 
+ * Release Date   : 24 May 2021
+ * Twitter        : https://twitter.com/galang_aidil, https://twitter.com/alfandi04_ 
+ * E-mail         : galangaidil45@gmail.com, yumardha@gmail.com, alfafandi0@gmail.com.
+-->
+
 <?php
+
 session_start();
-// Include functions and connect to the database using PDO MySQL
+
 include 'functions.php';
+
+/*
+EN : Connecting to the database using PHP Object Oriented Programming 
+ID : Menghubungkan ke database menggunakan PHP Pemrograman Berorientasi Objek
+*/
 require_once "config/database.php";
 $conn = new connectDB();
 $pdo = $conn->pdo_connect();
-// Page is set to home (home.php) by default, so when the visitor visits that will be the page they see.
+
+/* 
+EN : Page is set to home.php by default, so when the visitor visits website, home.php will be the page they see.
+ID : Halaman diatur ke home.php secara default, jadi ketika pengunjung mengunjungi situs, home.php akan menjadi halaman yang mereka lihat.
+*/
 $page = isset($_GET['page']) && file_exists($_GET['page'] . '.php') ? $_GET['page'] : 'home';
-// Include and show the requested page
+
+/*
+EN : Include and show the requested page
+ID : Sertakan dan tampilkan halaman yang diminta
+*/
 include $page . '.php';
