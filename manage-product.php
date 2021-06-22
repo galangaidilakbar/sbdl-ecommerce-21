ee09cd9d4d1c26d91cab61d235f3c645cbd190b6
Galangaidil <galangaidil45@gmail.com>
2021-06-22 21:46:00 +0700

    create comments

--- manage-product.php
+++ manage-product.php
@@ -1,21 +1,30 @@
+<!-- SHOPPINGCART
+ ****************************************************
+ * Developer Team : Galang aidil akbar, Mardha Yuda Kurniawan, Ahmad Sofiyan Alfandi. 
+ * Release Date   : 24 May 2021
+ * Twitter        : https://twitter.com/galang_aidil, https://twitter.com/alfandi04_ 
+ * E-mail         : galangaidil45@gmail.com, yumardha@gmail.com, alfafandi0@gmail.com.
+-->
+
 <?php
 class product
 {
   function recently_added()
   {
+    // create connection to database
     require_once "config/database.php";
     $conn = new connectDB();
-
     $pdo = $conn->pdo_connect();
 
+    //Prepare the query
     $query = "SELECT * FROM `recently_added`";
-
     $stmt = $pdo->prepare($query);
-
     $stmt->execute();
 
+    // Get the products
     $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
+    // Close Connection
     $conn = null;
     $stmt = null;
 
@@ -24,17 +33,20 @@ class product
 
   function show_product($id)
   {
+    // create connection to database
     require_once "config/database.php";
     $conn = new connectDB();
-
     $pdo = $conn->pdo_connect();
 
+    // Prepare the query
+    // The query is self is Stored Procedure
     $stmt = $pdo->prepare('CALL tampilkan_produk(?)');
-
     $stmt->execute([$id]);
 
+    // Get the product
     $product = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    // Close Connection
     $conn = null;
     $stmt = null;
 
@@ -43,17 +55,19 @@ class product
 
   function check_product($id)
   {
+    // create connection to database
     require_once "config/database.php";
     $conn = new connectDB();
-
     $pdo = $conn->pdo_connect();
 
+    // Prepare the query
     $stmt = $pdo->prepare('SELECT * FROM products WHERE id = ?');
-
     $stmt->execute([$id]);
 
+    // Get the product
     $product = $stmt->fetch(PDO::FETCH_ASSOC);
 
+    // Close Connection
     $conn = null;
     $stmt = null;
 
