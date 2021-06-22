fefd15dcc6d0f823bd52bf4d89ed1d35f69f9885
Galangaidil <galangaidil45@gmail.com>
2021-06-22 22:20:25 +0700

    fix icon path

--- register/index.php
+++ register/index.php
@@ -8,10 +8,10 @@
 	<!-- Icon and favicon start -->
 	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	<link href="style.css" rel="stylesheet" type="text/css">
-	<link rel="apple-touch-icon" sizes="180x180" href="../imgs/icon/apple-touch-icon.png">
-	<link rel="icon" type="image/png" sizes="32x32" href="../imgs/icon/favicon-32x32.png">
-	<link rel="icon" type="image/png" sizes="16x16" href="../imgs/icon/favicon-16x16.png">
-	<link rel="manifest" href="../imgs/icon/site.webmanifest">
+	<link rel="apple-touch-icon" sizes="180x180" href="../assets/icon/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="../assets/icon/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="../assets/icon/favicon-16x16.png">
+	<link rel="manifest" href="../assets/icon/site.webmanifest">
 	<!-- Icon and favicon End -->
 </head>
 
