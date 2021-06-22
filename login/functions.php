5445e80ca9bf5afe0957e948d2f3c477a2e7244d
Galangaidil <galangaidil45@gmail.com>
2021-06-22 22:26:23 +0700

    create comments

--- login/functions.php
+++ login/functions.php
@@ -1,3 +1,11 @@
+<!-- SHOPPINGCART
+ ****************************************************
+ * Developer Team : Galang aidil akbar, Mardha Yuda Kurniawan, Ahmad Sofiyan Alfandi. 
+ * Release Date   : 24 May 2021
+ * Twitter        : https://twitter.com/galang_aidil, https://twitter.com/alfandi04_ 
+ * E-mail         : galangaidil45@gmail.com, yumardha@gmail.com, alfafandi0@gmail.com.
+-->
+
 <?php
 // create function to load icon Website
 // please put this function inside head to work properly
