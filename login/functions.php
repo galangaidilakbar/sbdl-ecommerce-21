de7a6863cf56aa9f7a448bf6a113715c39bd5828
Galangaidil <galangaidil45@gmail.com>
2021-06-22 22:18:19 +0700

    fix icon src path

--- login/functions.php
+++ login/functions.php
@@ -4,10 +4,10 @@
 function load_icon()
 {
   echo <<<EOT
-  <link rel="apple-touch-icon" sizes="180x180" href="../imgs/icon/apple-touch-icon.png">
-  <link rel="icon" type="image/png" sizes="32x32" href="../imgs/icon/favicon-32x32.png">
-  <link rel="icon" type="image/png" sizes="16x16" href="../imgs/icon/favicon-16x16.png">
-  <link rel="manifest" href="../imgs/icon/site.webmanifest">
+  <link rel="apple-touch-icon" sizes="180x180" href="../assets/icon/apple-touch-icon.png">
+  <link rel="icon" type="image/png" sizes="32x32" href="../assets/icon/favicon-32x32.png">
+  <link rel="icon" type="image/png" sizes="16x16" href="../assets/icon/favicon-16x16.png">
+  <link rel="manifest" href="../assets/icon/site.webmanifest">
   EOT;
 }
 function mysqli_connect_to_database()
